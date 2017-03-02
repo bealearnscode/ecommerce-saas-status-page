3188c6f56bf324e6bb8ed657185713563b264aed
bealearnscode <bealearnscode@gmail.com>
2017-03-02 01:03:49 -0500

    testing different methods to get the json to render in html

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/*$(document).ready(function() {
+  $("button").click(function() {
+      $.getJSON("history.json", function(data) {
+          $.each(data, function(key, value) {
+              $("ul").append("<li>"+value.name+"</li>");
+          });
+      });
+  });
+});*/
+
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'history.json', true);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);
+ }
+
+ function init() {
+  loadJSON(function(response) {
+   // Parse JSON string into object
+     var actual_JSON = JSON.parse(response);
+  });
+ }
