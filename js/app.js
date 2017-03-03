75870ebf5094d7e3fe711cfb9373538c6bbf4322
bealearnscode <bealearnscode@gmail.com>
2017-03-02 23:04:24 -0500

    canvas chart is rendering both datasets!

--- js/app.js
+++ js/app.js
@@ -10,8 +10,28 @@ $(document).ready(function() {
                       + "</p></div>").appendTo("body");
                         console.log(key);
                     });
+
+                    drawLine();
+                      function drawLine() {
+                        var ctx = document.getElementById("myChart").getContext("2d");
+                        var myChart = new Chart(ctx, {
+                            type: "line",
+                            data: {
+                              labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13", "Day 14", "Day 15", "Day 16", "Day 17", "Day 18", "Day 19", "Day 20", "Day 21", "Day 22", "Day 23", "Day 24", "Day 25", "Day 26", "Day 27", "Day 28", "Day 29", "Day 30"],
+                              datasets: [{
+                                label: data.services[0].name,
+                                data: data.services[0].values,
+                                backgroundColor: "rgba(153,255,51,0.4)"
+                              }, {
+                                label: data.services[1].name,
+                                data: data.services[1].values,
+                                backgroundColor: "rgba(255,153,0,0.4)"
+                            }]
+                            }
+                      })
+                    };
             });
-            drawLine();
+            /*drawLine();
               function drawLine() {
                 var ctx = document.getElementById("myChart").getContext("2d");
                 var myChart = new Chart(ctx, {
@@ -29,5 +49,5 @@ $(document).ready(function() {
                     }]
                     }
               })
-            };
+            };*/
 });
