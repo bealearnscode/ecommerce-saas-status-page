1e00f60612fd76a294fd22ad68206d8951130fb4
bealearnscode <bealearnscode@gmail.com>
2017-03-03 20:37:38 -0500

    got both charts showing! had to remove 2d context

--- js/app.js
+++ js/app.js
@@ -12,8 +12,10 @@ $(document).ready(function() {
                     });
 
                     drawBar30();
+                    drawBar7();
+
                       function drawBar30() {
-                        var ctx = document.getElementById("myChart30").getContext("2d");
+                        var ctx = document.getElementById("myChart30");
                         var myChart30 = new Chart(ctx, {
                             type: "bar",
                             data: {
@@ -30,5 +32,24 @@ $(document).ready(function() {
                             }
                       })
                     };
+
+                    function drawBar7() {
+                      var ctx = document.getElementById("myChart7");
+                      var myChart7 = new Chart(ctx, {
+                          type: "bar",
+                          data: {
+                            labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
+                            datasets: [{
+                              label: data.services[0].name,
+                              data: data.services[0].values,
+                              backgroundColor: "rgba(52, 152, 219,0.4)"
+                            }, {
+                              label: data.services[1].name,
+                              data: data.services[1].values,
+                              backgroundColor: "rgba(219,119,52,0.4)"
+                          }]
+                          }
+                    })
+                  };
             });
 });
