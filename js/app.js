15c7167386e201fb6c9817478e1e0bfaa1eb81a5
bealearnscode <bealearnscode@gmail.com>
2017-03-03 20:09:53 -0500

    change id name of chart

--- js/app.js
+++ js/app.js
@@ -13,8 +13,8 @@ $(document).ready(function() {
 
                     drawBar();
                       function drawBar() {
-                        var ctx = document.getElementById("myChart").getContext("2d");
-                        var myChart = new Chart(ctx, {
+                        var ctx = document.getElementById("myChart30").getContext("2d");
+                        var myChart30 = new Chart(ctx, {
                             type: "bar",
                             data: {
                               labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13", "Day 14", "Day 15", "Day 16", "Day 17", "Day 18", "Day 19", "Day 20", "Day 21", "Day 22", "Day 23", "Day 24", "Day 25", "Day 26", "Day 27", "Day 28", "Day 29", "Day 30"],
