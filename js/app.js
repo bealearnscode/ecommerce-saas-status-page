f6186b90be3d527a1d2691dcac1981de1d3d6b06
bealearnscode <bealearnscode@gmail.com>
2017-03-06 22:47:28 -0500

    try putting inner div in nav to fix position

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
     $("nav").wrap("<div class='nav-placeholder'></div>");
     $(".nav-placeholder").height($("nav").outerHeight());
 
+    $("nav").wrapInner("<div class='nav-inner'></div>");
+
     $(window).scroll(function() {
       var scrollPos = $(window).scrollTop();
       if (scrollPos >= navOffset) {
