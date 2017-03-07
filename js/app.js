c7fce9cfa6a07dbd5283637cfc74c69d75c40619
bealearnscode <bealearnscode@gmail.com>
2017-03-06 22:21:53 -0500

    use jquery to create a parent div for the nav to prevent it from rendering outside normal flow of document

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
 
     var navOffset=$("nav").offset().top;
 
+    $("nav").wrap("<div class='nav-placeholder'></div>");
+    $(".nav-placeholder").height($("nav").outerHeight());
+
     $(window).scroll(function() {
       var scrollPos = $(window).scrollTop();
       if (scrollPos >= navOffset) {
